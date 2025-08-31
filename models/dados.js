e01b8ed9f8a91a5f5485a26c0fc25fb03d471859
Priscila Nascimento <priskaurdi@gmail.com>
2025-08-31 08:40:07 -0300

    Testando a página index.html com o arquivo temperaturas_100.json

--- models/dados.js
+++ models/dados.js
@@ -1,11 +1,19 @@
-const mongoose = require('mongoose'); // 885.9k (gzipped: 237.2k)
+//models/dados.js
+//Esse arquivo define o modelo de dados no MongoDB com Mongoose.
 
+
+// Importa o Mongoose para conectar e manipular MongoDB
+const mongoose = require('mongoose'); 
+
+// Estrutura dos dados armazenados no banco
 const dadosSchema = new mongoose.Schema({
-    temperature: Number ,
-    timestamp :{type: Date, default: Date.now}
+    temperature: Number ,                       // Valor da temperatura
+    timestamp :{type: Date, default: Date.now}  // Data e hora da leitura (default = agora)
 });
 
+// Cria o "modelo" Dados com base no schema
 const Dados = mongoose.model('Dados', dadosSchema);
 
+// Exporta para poder usar no server.js
 module.exports = Dados;
 
